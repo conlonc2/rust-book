0bd5f9c4ca2dcf5627e7b9b9167ae1faac663372
Chris Conlon <chris@coding-ninja.com>
2023-02-14 21:03:03 -0500

    [guessing-game] - [Chapter-2] - Initial funtionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Start up messages
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    
+    // Declare a mutable variable
+    let mut guess = String::new();
+    
+    // Grab the input from standard in (user input). Expect a failure if something went wrong
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the incomming line.");
+
+    println!("You guessed: {guess}");
 }
