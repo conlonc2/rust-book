373122c6a4017819f35d4b5ee30a6abd9d06c19e
Chris Conlon <chris@coding-ninja.com>
2023-02-14 21:35:45 -0500

    [guessing-game] - [Chapter-2] - guessing game complete

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io::{self, Write}};
 
 fn main() {
+    // Declare a mutable variable
+    // Generate a random number
+    let random_number = rand::thread_rng().gen_range(1..=100);
+
     // Start up messages
     println!("Guess the number!");
-    println!("Please input your guess.");
-    
-    // Declare a mutable variable
-    let mut guess = String::new();
-    
-    // Grab the input from standard in (user input). Expect a failure if something went wrong
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the incomming line.");
+    println!("The Generated Number Was: {random_number}");
 
-    println!("You guessed: {guess}");
+    loop {
+        let mut guess = String::new();
+        print!("Please input your guess: ");
+        io::stdout().flush().expect("Something went wrong flushing the stdout"); // Helps collect
+                                                                                 // data with
+                                                                                 // prompt inline
+        // Grab the input from standard in (user input). Expect a failure if something went wrong
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the incomming line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("Please pick a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("To small!"),
+            Ordering::Greater => println!("To big!"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
+    }
 }
